eeef32584ea3ada40ec8a5a39f57bf3c5d3d5ad3
Julius Diestra <julius.diestra.cabrera@gmail.com>
2022-02-12 12:22:55 +0100

    Updates in LU Decomposition method: L and U matrices updates

--- linear-algebra/include/marsvin_ludecomposition.hpp
+++ linear-algebra/include/marsvin_ludecomposition.hpp
@@ -16,6 +16,8 @@ class LUDecomposition {
 
 
 template<typename T> std::vector<marsvin::Matrix<T>> marsvin::LUDecomposition::PartialPivoting(const marsvin::Matrix<T>& A) {
+    // Reference:
+    // [1] Page 237 -> Numerical Linear Algebra: Theory and Applications - Larisa Beilina, Evgenii Karchevskii and Mikhail Karchevskii
     std::vector<marsvin::Matrix<T>> result_;
     if (m1.IsSquare()) {
         // Initial condition
@@ -29,10 +31,19 @@ template<typename T> std::vector<marsvin::Matrix<T>> marsvin::LUDecomposition::P
                 P.SwapRows(m,j);
                 U.SwapRows(m,j);
                 if (j >= 2) {
-                    
+                    std::vector<T> new_row_j {A.GetRow(m).begin(), A.GetRow(m).begin()+j-2};
+                    new_row_j.insert(new_row_j.end(),A.GetRow(m).begin() + j - 1,A.GetRow(m).end());
+                    std::vector<T> new_row_m {A.GetRow(j).begin(), A.GetRow(j).begin()+j-2};
+                    new_row_m.insert(new_row_m.end(),A.GetRow(j).begin() + j - 1,A.GetRow(j).end());
+                    A.SetRow(j,new_row_j); 
+                    A.SetRow(m,new_row_m); 
                 }
             }
-            
+            // Update L
+            std::vector<T> new_L_column_j { L.GetColumn(j).begin(),L.GetColumn(j.begin()+j-1) };
+            std::vector<T> L_sub_column = std::transform(U.GetColumn(j).begin(),U.GetColumn(j).end(),[U,j](T &c){return c/U.GetEntry(j,j)});
+            // Ref: https://slaystudy.com/c-divide-vector-by-scalar/
+            // Update U
         }
     }
 }
